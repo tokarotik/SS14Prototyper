83854efb2523a1f000d982197f03be765ece53f3
ToKarotik <markzhiv@gmail.com>
2026-08-02 20:57:08 +0300

    making config + launch script + maked better the readme

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,3 @@
-try:
-	from argparse import ArgumentParser
-except ModuleNotFoundError:
-	print("You don't have a module 'argparse'\nPlease install this 'pip install argparse'")
+from argparse import ArgumentParser
+
 
